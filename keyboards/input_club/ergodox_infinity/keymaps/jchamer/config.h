d8d6ecb68fad8849b1ee6e3914ced0a92fa6ecdc
Jonathan Hamermesh <7157168+JCHamer@users.noreply.github.com>
2024-05-25 15:33:44 -0400

    Cleaning up stale config.h entries for the ErgoDox Infinity jchamer layout.

--- keyboards/input_club/ergodox_infinity/keymaps/jchamer/config.h
+++ keyboards/input_club/ergodox_infinity/keymaps/jchamer/config.h
@@ -1,16 +1,8 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-//#include "../../config.h"
-
-//#undef  PERMISSIVE_HOLD
-// #define PERMISSIVE_HOLD
-
 #define EE_HANDS
 
-#define ONESHOT_TAP_TOGGLE       2
-
-//#undef  TAPPING_TERM
 #define TAPPING_TERM			200
 
 #define QUICK_TAP_TERM			100
